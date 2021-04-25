7ae4ac3f4c888a188fbd2c8f24a71135307187f9
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-25 13:11:21 +0530

    Added: Welcome to the cab invoice generartor

---
+++ src/main/java/com/invoicegenerator/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.invoicegenerator;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the cab invoice generator");
+    }
+}
