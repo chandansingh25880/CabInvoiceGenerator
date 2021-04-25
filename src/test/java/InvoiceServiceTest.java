29677a0d43a377cec99bd92a7773e663bcb64c19
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-25 15:34:36 +0530

    Added: To calculate the totalfare and minfare by given data

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -13,4 +13,14 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25, fare, 0.0);
 
     }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.2;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+
+    }
 }
