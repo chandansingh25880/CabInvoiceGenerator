0c80228b618c9e0877288da7ef91ece4dc6975a9
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-25 13:55:43 +0530

    Added: To calculate the totalfare and minfare by given data

--- src/main/java/com/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/invoicegenerator/InvoiceGenerator.java
@@ -1,8 +1,16 @@
 package com.invoicegenerator;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
 
-        System.out.println("Welcome to the cab invoice generator");
+    private static final double MINIMUN_COST_PER_KILOMETER = 10;
+    private static final double MINIMUN_FARE = 5;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare =  distance * MINIMUN_COST_PER_KILOMETER  + time * COST_PER_TIME ;
+        if(totalFare < MINIMUN_FARE )
+            return  MINIMUN_FARE;
+        return totalFare;
+
     }
 }
